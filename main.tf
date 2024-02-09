b8d430f101a670a36edf2390c66b0766b1d281ff
cocoa-maemae <breezin_ahimusa@aol.com>
2024-02-09 20:11:02 +0900

    add aws secrets

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = "ap-northeast-1"
+}
+
 module "vpc" {
   source = "git::https://github.com/cocoa-maemae/terraform-aws-vpc-testmodule.git"
 }
